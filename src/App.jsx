8f3c08937b2fb2cafc3b82036af29bddd346009f
devlucasnascimento81 <dev.lucasnascimento081@gmail.com>
2025-12-23 18:00:31 -0300

    feat: nova função para completar as tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -32,28 +32,35 @@ function App() {
       category,
       isCompleted: false
     }
-  ]
+    ]
 
-  setTodos(newTodos)
+    setTodos(newTodos)
   }
 
-  const  removeTodo = (id) => {
+  const removeTodo = (id) => {
     const newTodos = [...todos]
     const filteredTodos = newTodos.filter(todo => todo.id !== id ? todo : null)
     setTodos(filteredTodos)
   }
-    
 
-  return <div className="app">
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo)
+    setTodos (newTodos)
+  }
+
+
+  return (
+    <div className="app">
       <h1>Lista de Tarefas</h1>
-      <div  className="todo-list">
+      <div className="todo-list">
         {todos.map((todo) => (
-         <Todo key={todo.id} todo={todo} removeTodo={removeTodo}/> 
+          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
         ))}
       </div>
-      <TodoForm addTodo={addTodo}/>
-    </div>
-  
+      <TodoForm addTodo={addTodo} />
+    </div>)
+
 }
 
 export default App
