e89898ba965202cc1c5393d0297d996f25b8bca5
devlucasnascimento81 <dev.lucasnascimento081@gmail.com>
2025-12-24 11:46:52 -0300

    feat: nova função de ordem alfabética

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,9 @@ function App() {
 
   const [search, setSearch] = useState("")
 
+  const [filter, setFilter] = useState("All")
+  const [sort, setSort] = useState("Asc")
+
   const addTodo = (text, category) => {
     const newTodos = [...todos, {
       id: Math.floor(Math.random() * 10000),
@@ -50,28 +53,34 @@ function App() {
   const completeTodo = (id) => {
     const newTodos = [...todos]
     newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo)
-    setTodos (newTodos)
+    setTodos(newTodos)
   }
 
 
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
-      <Search search={search} setSearch={setSearch}/>
-      <Filter/>
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
         {todos
-          .filter((todo) => 
+          .filter((todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted)
+          .filter((todo) =>
             todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) => 
+            sort === "Asc" 
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
             )
-            .map((todo) => (
-              <Todo
-                key={todo.id}
-                todo={todo}
-                removeTodo={removeTodo}
-                completeTodo={completeTodo}
-              />
-        ))}
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo}
+            />
+          ))}
       </div>
       <TodoForm addTodo={addTodo} />
     </div>)
